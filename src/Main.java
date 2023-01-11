b1629da9e7c1af11b74e81b52796c550f68b8cde
Felipe Zanardo <felipebueno6@hotmail.com>
2023-01-11 09:10:54 -0300

    criação do menu principal usando a classe utilitária ConsoleUIHelper

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import view.MenuView;
+
 public class Main {
     public static void main(String[] args) {
-        
+        MenuView.iniciarMenuPrincipal();
+        System.out.println("\nFim do programa!\n");
     }
 }
---
+++ src/controller/MenuController.java
@@ -0,0 +1,24 @@
+package controller;
+
+public class MenuController {
+    public static void direcionarMenu(int opcao){
+        switch (opcao){
+            case 0 -> {}    //método da opção 0 do menu
+            case 1 -> {}
+            case 2 -> {}
+            case 3 -> {}
+            case 4 -> {}
+            case 5 -> {}
+            case 6 -> {}
+            case 7 -> {}
+            case 8 -> {}
+            case 9 -> {}
+            case 10 -> {}
+            case 11 -> {}
+            case 12 -> {}
+            case 13 -> {}
+            default -> System.out.println("\nErro desconhecido\n");         //tratamento de erro feito por ConsoleUIHelper
+        }
+    }
+
+}
